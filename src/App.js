e4c5b8aaa011afa9ac56615cb4b7c15f501b1bb4
shubham khulbe <skhulbe75@gmail.com>
2021-09-06 19:11:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import alanBtn from '@alan-ai/alan-sdk-web'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./styles.js";
+
+const alanKey = '57f28c58b2b8f4b858dbefe51ecd14502e956eca572e1d8b807a3e2338fdd0dc/stage';
+const App = () =>{
+
+    const[ newsArticles, setNewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
+    const classes = useStyles();
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({command, articles}) =>{
+                if(command === 'newHeadlines'){
+                    setNewsArticles(articles);
+                    setActiveArticle(-1);
+                }else if(command === 'highlight'){
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1)
+                }
+            }
+
+        })
+    } ,[])
+
+    return(
+        <div>
+            <div>
+                <div className={classes.logoContainer}>
+                    <img src="https://46ba123xc93a357lc11tqhds-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/alan.jpg" className={classes.alanLogo} alt="alan logo" />
+                </div>
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+        </div>
+
+    );
 }
 
 export default App;
